f2fe502434ff6e5c45cf81172d551eb67fff0b11
EKREMIE <ilgar.ekrem@gmail.com>
2023-11-02 11:17:13 +0100

    intégration du fichier json et style des cards

--- src/Components/GalleryItem.js
+++ src/Components/GalleryItem.js
@@ -1,14 +1,13 @@
-// GalleryItem.js
-
+import '../Styles/GalleryItem.css'
 import React from 'react';
 import PropTypes from 'prop-types';
 
 function GalleryItem({ title }) {
     return (
         <div className="gallery-item">
-            {/* Afficher le titre de location */}
+
             <h3>{title}</h3>
-            {/* Ajouter d'autres éléments de la carte ici */}
+
         </div>
     );
 }
