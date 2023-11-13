4bd313fe27694bd2141c8c368967d0ea71a6d535
EKREMIE <ilgar.ekrem@gmail.com>
2023-11-13 14:49:22 +0100

    finitions

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,8 @@
-// Home.js
+
 import React from 'react';
 import Gallery from '../Components/Gallery/Gallery';
 import Header from "../Components/Header/Header";
 import Footer from "../Components/Footer/Footer";
-import './Home.scss';
 
 function Home() {
     return (
